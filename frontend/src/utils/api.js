9332966f29dfd59bb0997712e434628fbd2a60cd
SamCs404 <samcserror404@gmail.com>
2026-01-17 19:30:30 +0530

    Make API config environment-aware with JWT support

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { getToken } from "./auth";
 
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+
 const api = axios.create({
-  baseURL: "http://localhost:4000/api"
+  baseURL: `${BASE_URL}/api`
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = getToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -14,3 +17,4 @@ api.interceptors.request.use(config => {
 });
 
 export default api;
+
